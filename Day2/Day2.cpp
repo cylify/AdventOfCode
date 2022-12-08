ae54b8ff57a924a02ef7edeaf6a05f87704617a7
cylify <mradi.lachman@gmail.com>
2022-12-08 14:04:18 -0500

    Day2

---
+++ Day2/Day2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int fight(string battle) {
+}
+
+int main() {
+    ifstream day2("day2Input.txt");
+    string battle;
+    while(getline(day2, battle)) {
+
+    }
+}
